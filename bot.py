c8f019a13fa6cb871d5d3516bb651bc9b2264507
Sergey Golikov <serp_13@mail.ru>
2024-02-21 23:36:00 +0300

    Мой бот

--- bot.py
+++ bot.py
@@ -1,27 +1,38 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import ephem
+import datetime
 import settings
 
-logging.basicConfig(filename="bot.log", level=logging.INFO)
-
-PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
-
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 def greet_user(update, context):
     print("Вызван /start")
-    update.message.reply_text("Здравствуй, пользователь!")
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+def planet_ephem(update, context):
+    user_text = update.message.text
+    planet_name = user_text.split()
+    current_date = datetime.date.today().isoformat()
+    new_planet = ephem.planet_name[2](current_date)
+    try:
+        update.message.reply_text(new_planet)
+    except:
+        update.message.reply_text('Введите название реальной планеты на английском с помощью команды /planet "название планеты"')
+
+
 
 def talk_to_me(update, context):
-    text = update.message.text
-    print(text)
-    update.message.reply_text(text)
-
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY)
+
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_ephem))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
